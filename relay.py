d514c32bb4f537b71edc18191da7cedac2d1d659
109550172 <axzzreety.cs09@nctu.edu.tw>
2020-12-11 21:32:45 +0800

    Changes to be committed: 	modified:   relay.py

--- relay.py
+++ relay.py
@@ -1,13 +1,13 @@
 import time
 import RPi.GPIO as GPIO
-relay = 21
+relay = 4
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(relay,GPIO.OUT)
 try :
     while True:
         GPIO.output(relay,GPIO.HIGH)
-        time.sleep(1)
+        time.sleep(10)
         GPIO.output(relay,GPIO.LOW)
-        time.sleep(1)
+        time.sleep(5)
 except KeyboardInterrupt :
     print("close")
